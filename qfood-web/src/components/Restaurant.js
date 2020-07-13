1e5c72356c6297063eeae1a6b29e5811095646df
Andrew Moskovets <moskovecandrey@gmail.com>
2020-07-13 16:23:38 +0300

    add flask back

--- qfood-web/src/components/Restaurant.js
+++ qfood-web/src/components/Restaurant.js
@@ -3,46 +3,50 @@ import './Restaurant.css';
 import MenuBlock from './MenuBlock';
 
 class Restaurant extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {data: null};
+    }
+
+    rest_name = this.props.match.params.rest_name;
+
+    uploadHandler = () => {
+        fetch('http://localhost:5000/find_rest/' + this.rest_name, {
+          method: 'GET',
+        })
+          .then((response) => response.json())
+          .then((success) => {
+            this.setState({ data: success })
+          })
+          .catch((error) => {
+            // eslint-disable-next-line no-console
+            console.log(error);
+          });
+    };
+
     render() {
 
-        const data = {'rest_name':'Best Food', 'blocks':[{'name':'PIZZA', 'products':[
-            {'type':'small_title', 'text':'Classic pizza'},
-            {'type':'price_label', 'text':'small/large'},
-            {'type':'meal', 'name':'Margherita', 'price':'5$/7$', 'description':'Tomato, salami, mashrooms, peperoni, salami'},
-            {'type':'meal', 'name':'Margherita', 'price':'5$/7$', 'description':'Tomato, salami, mashrooms, peperoni, salami'},
-            {'type':'meal', 'name':'Margherita', 'price':'5$/7$', 'description':'Tomato, salami, mashrooms, peperoni, salami'},
-            {'type':'small_title', 'text':'Not classic pizza'},
-            {'type':'meal', 'name':'Margherita', 'price':'5$/7$', 'description':'Tomato, salami, mashrooms, peperoni, salami'},
-            {'type':'meal', 'name':'Margherita', 'price':'5$/7$', 'description':'Tomato, salami, mashrooms, peperoni, salami'}]
-         }, 
-         {'name':'PIZZA', 'products':[
-            {'type':'small_title', 'text':'Classic pizza'},
-            {'type':'price_label', 'text':'small/large'},
-            {'type':'meal', 'name':'Margherita', 'price':'5$/7$', 'description':'Tomato, salami, mashrooms, peperoni, salami'},
-            {'type':'meal', 'name':'Margherita', 'price':'5$/7$', 'description':'Tomato, salami, mashrooms, peperoni, salami'},
-            {'type':'meal', 'name':'Margherita', 'price':'5$/7$', 'description':'Tomato, salami, mashrooms, peperoni, salami'},
-            {'type':'small_title', 'text':'Not classic pizza'},
-            {'type':'meal', 'name':'Margherita', 'price':'5$/7$', 'description':'Tomato, salami, mashrooms, peperoni, salami'},
-            {'type':'meal', 'name':'Margherita', 'price':'5$/7$', 'description':'Tomato, salami, mashrooms, peperoni, salami'}
-         ]}
-        ]};
+        const { data } = this.state;
+               
+        if (data != null) {
+            const renderBlocks = data['blocks'].map((block, i) => {
+                return (
+                    <MenuBlock block={block} key={i}/>
+                );
+            });
 
-        // const rest_name = this.props.match.params.rest_name;
-
-        const renderBlocks = data['blocks'].map((block, i) => {
             return (
-                <MenuBlock block={block} key={i}/>
-            );
-        });
-
-        return (
-            <div>
-                <div className='header'>{data['rest_name']}</div>
-                <div className='menu_column'>
-                    {renderBlocks}
+                <div>
+                    <div className='header'>{data['rest_name']}</div>
+                    <div className='menu_column'>
+                        {renderBlocks}
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+            this.uploadHandler();
+            return (<p>Loading...</p>)
+        }
     }
 }
 

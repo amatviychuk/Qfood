80e5fbef3fc6f15bcd7ae75b2cdf5390a6a938ff
Andrew Moskovets <moskovecandrey@gmail.com>
2020-07-16 21:20:20 +0300

    Updates

---
+++ precache-manifest.ff1ecbb8e4bad730a38da53392b6ee30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eb2563ddfc74e6f52b19bb6be12bd99",
+    "url": "/Qfood/index.html"
+  },
+  {
+    "revision": "c860d221ad1450c91379",
+    "url": "/Qfood/static/css/main.a3e1f916.chunk.css"
+  },
+  {
+    "revision": "70688a608308d1225357",
+    "url": "/Qfood/static/js/2.3f1a9557.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Qfood/static/js/2.3f1a9557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c860d221ad1450c91379",
+    "url": "/Qfood/static/js/main.9059e745.chunk.js"
+  },
+  {
+    "revision": "ca09de81a4a74d87aab4",
+    "url": "/Qfood/static/js/runtime-main.0af91d72.js"
+  }
+]);

c7964c170bcbea3f094547254eeb0111b4c26e9c
Andrew Moskovets <moskovecandrey@gmail.com>
2020-07-18 22:41:34 +0300

    Updates

---
+++ precache-manifest.ec6dcc97e9846c4e18327d96d2e14dc7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5132b677a9f08eea9633c7ac843cab02",
+    "url": "/Qfood/index.html"
+  },
+  {
+    "revision": "4aafc621909e0cc92e98",
+    "url": "/Qfood/static/css/main.a3e1f916.chunk.css"
+  },
+  {
+    "revision": "26ebe4331afd018a8aa3",
+    "url": "/Qfood/static/js/2.60e66a69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Qfood/static/js/2.60e66a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4aafc621909e0cc92e98",
+    "url": "/Qfood/static/js/main.84aeddc0.chunk.js"
+  },
+  {
+    "revision": "ca09de81a4a74d87aab4",
+    "url": "/Qfood/static/js/runtime-main.0af91d72.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.38184eb814766a55845b64d752aecb8d.js"
+  "/Qfood/precache-manifest.ec6dcc97e9846c4e18327d96d2e14dc7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Qfood/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
